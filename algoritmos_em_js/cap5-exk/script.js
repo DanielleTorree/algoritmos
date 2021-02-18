9d5120fcd4742f012617d815f5d6407c27708122
Danielle Torres de Freitas <danielletorree01@gmail.com>
2021-02-18 20:15:09 -0300

    algoritmo desenvolvido em js

---
+++ algoritmos_em_js/cap5-exk/script.js
@@ -0,0 +1,16 @@
+function executar(){
+    var res = document.getElementById('res')
+
+    var grao = 0
+    var quadro = 1
+
+    for(var i = 1; i <= 64; i++){
+       
+       grao = grao + quadro
+       
+       
+       quadro = quadro * 2
+
+       res.innerHTML += `${grao} <br>`
+    }
+}
