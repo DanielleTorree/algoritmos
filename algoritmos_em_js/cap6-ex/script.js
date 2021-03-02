f7e5b4d86ff84c1a4a3c0cfa18b993e4fd5467d8
Danielle Torres de Freitas <danielletorree01@gmail.com>
2021-03-02 16:21:56 -0300

    array

---
+++ algoritmos_em_js/cap6-ex/script.js
@@ -0,0 +1,20 @@
+function executar(){
+    var soma = 0
+    var n = []
+    var res = window.document.getElementById('res')
+
+    //4.5, 6.5, 8, 3.5, 6, 6.5, 6.5, 6
+
+    for(var i = 0; i <= 7; i++){
+        //n.push( )
+        
+
+        soma = soma + Number(window.prompt('Digite a média do aluno: '))
+        
+        console.log(soma)
+    }
+
+    var media = soma / 8
+    
+    window.alert(`Esta é a média: ${media}`)
+}
