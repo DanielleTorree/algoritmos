c294989f3a9733c43207a3a281480fba6e30a845
Danielle Torres de Freitas <danielletorree01@gmail.com>
2021-03-04 14:49:49 -0300

    algoritmos

--- algoritmos_em_js/cap6-exemplo2/script.js
+++ algoritmos_em_js/cap6-exemplo2/script.js
@@ -1,41 +1,17 @@
 function executar(){
-    var array = []
+    var res = document.getElementById('res')
 
-    for(var i = 1; i <= 5; i++){
-        array.push(window.prompt('Digite um número: '))
-        console.log(array)
+    var array = [15, 20, 35, 40]
+    var total = 0
+
+    res.innerHTML = `${array.length} <br>`
+
+    for(var i = 0; i <= 3 /*array.length*/; i++){
+        
+        if(array[i] % 2 != 0){
+            total = total + array[i]
+        }
     }
 
-    console.log(array[3]);
-    console.log('vamos para o segundo...');
-
-    while (array.length != 0){
-        console.log(array);
-        array.pop();
-    }
-
-    var arr = [{nome:'Danielle Linda', idade:21}]
-
-    arr.push({nome:'Alex Lindo', idade:46});
-    arr.push([100,99,12]);
-    arr.push({vaiculo:'Carro', ano:2021, placa:'AK59'});
-    arr.push([{lingua:'Inglês', nivel:1, tipo:'Iniciante'}, 
-          {lingua:'Português', nivel:2, tipo:'Intermediário'}])
-
-    //console.log(arr);
-    console.log(arr[0]);
-    console.log(arr[0].nome);
-    console.log(arr[0].idade);
-    console.log(arr[2][1]);
-    console.log(arr[4][1].lingua);
-    //console.log(arr.length);
-    
-    for(var i = 0;i <= (arr.length-1); i++){
-        console.log(arr[i]);
-    }
-
-    //(arr.length-1) é a posição
-
-
-
+    res.innerHTML += total
 }
