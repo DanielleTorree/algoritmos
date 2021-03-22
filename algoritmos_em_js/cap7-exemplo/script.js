0c0d7cb713b6c8bd50869ea4cae95477ca4346c1
Danielle Torres de Freitas <danielletorree01@gmail.com>
2021-03-22 18:42:39 -0300

    atualização

--- algoritmos_em_js/cap7-exemplo/script.js
+++ algoritmos_em_js/cap7-exemplo/script.js
@@ -1,18 +1,9 @@
 function executar(){
-    var nome = []
-    var x = ''
+    var lista = ["Danielle", 15, 32, 4, "JavaScript"]
 
     for(var i = 0; i < 5; i++){
-        nome.push(window.prompt('Digite um nome aqui: '))
-    }
-
-    for(var i = 0; i < 5; i++){
-        if(nome[i] > nome[i+1]){
-            x = nome[i]        
-            nome[i] = nome[i+1]
-            nome[i+1] = x
+        if(lista[i] == 32){
+            console.log(`Elemento ${lista[i]} encontrado na posição ${i}`)
         }
-    }
-    
-    console.log(nome)    
+    }    
 }
