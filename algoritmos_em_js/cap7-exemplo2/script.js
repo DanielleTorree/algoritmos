51db126b0bb2437b85c64ee0457097d4bc1e67e2
Danielle Torres de Freitas <danielletorree01@gmail.com>
2021-03-24 19:56:23 -0300

    atualizações

--- algoritmos_em_js/cap7-exemplo2/script.js
+++ algoritmos_em_js/cap7-exemplo2/script.js
@@ -2,30 +2,33 @@ function executar(){
     var nome = []
 
     for(var i = 0; i < 5; i++){
-        nome.push(window.prompt('Digite um nome aqui: '))
+        nome.push(window.prompt('Digite um nome: '))
     }
 
     do{
-        var pesquisa = window.prompt('Qual nome você deseja pesquisar: ')
 
+        var pergNome = window.prompt('Qual nome deseja pesquisar?')
+        
         var i = 0
-        var acha = false
+        var achaNome = false
+        //var achaNome = false ====> é uma variável flag (bandeira)
 
-        while(i < 5 && acha == false){
-            if(pesquisa == nome[i]){
-                acha = true
+        while(i < 5 && achaNome == false){
+
+            if(pergNome == nome[i]){
+                achaNome = true
             } else{
                 i++
             }
         }
 
-        if(acha == true){
-            window.alert(`${pesquisa} foi encontrada na posição ${i}`)
+        if(achaNome == true){
+           window.alert(`O nome ${pergNome} foi encontrado na posição ${i}.`) 
         } else{
-            window.alert(`${pesquisa} não foi encontrada`)
+            window.alert(`O nome ${pergNome} não foi encontrado.`)
         }
 
-       var pergUsu = window.prompt('Você deseja continuar: ')
-
-    } while(pergUsu == 'sim')
+        var perg = window.prompt('Deseja continuar? (SIM/NÃO):')
+    
+    }while(perg == 'sim' || perg == 'Sim' || perg == 'SIM')
 }
---
+++ algoritmos_em_js/cap7-exemplo3/script.js
@@ -0,0 +1,24 @@
+function executar(){
+    var nome = []
+
+    for(var i = 0; i < 5; i++){
+        nome.push(window.prompt('Digite um nome: '))
+    }
+
+    for(var i = 0; i < nome.length; i++){
+        var min = i
+
+        for(var j = i + 1; j < nome.length; j++){
+
+            if(nome[min] > nome[j]){
+                min = j
+            }
+        }
+            
+        var temp = nome[i]
+        nome[i] = nome[min]
+        nome[min] = temp
+    }
+
+    console.log(nome)
+}
