d62c69ceddb0c1ae80260e0f852c55f85880b0da
Danielle Torres de Freitas <danielletorree01@gmail.com>
2021-03-25 21:53:32 -0300

    Update script.js

--- algoritmos_em_js/cap7-exemplo3/script.js
+++ algoritmos_em_js/cap7-exemplo3/script.js
@@ -20,5 +20,31 @@ function executar(){
         nome[min] = temp
     }
 
-    console.log(nome)
+    do{
+        var pergNome = window.prompt('Qual nome você deseja encontrar? ')
+
+        var inicio = 1
+        var fim =  5
+        var meio = (inicio + fim)/2
+        var achaNome = false
+
+        while(inicio <= fim && achaNome == false){
+            if(pergNome == nome[parseInt(meio)]){
+                achaNome = true
+            } else if(pergNome  < nome[parseInt(meio)]){
+                fim = meio - 1
+            } else{
+                fim = meio + 1
+            }
+        }
+
+        if(achaNome == true){
+            window.alert(`O nome ${perNome} foi localizafo na posição ${meio}`)
+        } else{
+            window.alert(`O nome ${pergNome} não foi localizado`)
+        }
+
+        var resp = window.prompt('Deseja continuar? [sim/não]')
+
+    } while(resp == 'sim')
 }
